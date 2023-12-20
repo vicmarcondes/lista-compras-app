0523785e0ff88d214674828daed86871c0a63183
Victor Moreno <victorgalves@hotmail.com>
2023-12-20 19:18:04 -0300

    Initial Commit

---
+++ ListaComprasApp/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListaComprasApp
+//
+//  Created by admin on 20/12/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
